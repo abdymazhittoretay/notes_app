e1c5a44338dc72f291071b85a55a70567b0117d3
Toretay <abdymvzhit@gmail.com>
2025-01-24 22:24:30 +0500

    added floatingActionButton in home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,6 +15,16 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        shape: CircleBorder(),
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 30.0,
+        ),
+      ),
     );
   }
 }
