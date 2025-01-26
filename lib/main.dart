32a65be9b94dab024562db6ac8197185ddd468b0
Toretay <abdymvzhit@gmail.com>
2025-01-26 14:48:58 +0500

    initialized firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/firebase_options.dart';
 import 'package:notes_app/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
